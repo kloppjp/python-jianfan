2b5adde96de3871dafbad44c9c63b5db83e06d3a
leon dong <leon.dong@gmail.com>
2013-05-10 08:04:37 +0000

    rev to 0.0.2 to support Python3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='jianfan',
-      version='0.0.1',
+      version='0.0.2',
       description='Jianfan is a library for translation between traditional and simplified chinese',
       author='Leon Dong',
       author_email='Leon.Dong@gmail.com',
